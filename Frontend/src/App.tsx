e82311dc76a1600ef1b02f95349fd23511178671
Athos Danilo <athosdanilomarques@gmail.com>
2026-08-31 12:58:28 -0300

    Rotas das Páginas

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,32 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import ProdutoDetalhes from './pages/ProdutoDetalhes';
+import Carrinho from './pages/Carrinho';
+import Checkout from './pages/Checkout';
+import StatusCompra from './pages/StatusCompra';
+import ComoFiz from './pages/ComoFiz';
+import Catalogo from './pages/Catalogo';
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <main>
-      <h1>Maratona - Vitrine em Construção</h1>
-    </main>
-  )
+    <BrowserRouter>
+      <Header />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/produto/:id" element={<ProdutoDetalhes />} />
+          <Route path="/carrinho" element={<Carrinho />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/status-compra" element={<StatusCompra />} />
+          <Route path="/como-fiz" element={<ComoFiz />} />
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
